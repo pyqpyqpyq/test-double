4b567f3ab3fdef1d354979ce3c23bc55ff92e7f4
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-16 02:41:21 +0800

    stub_of_test_double

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,28 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-
+    @Mock
+    private GradeService mockGradeService;
+    @InjectMocks
+    private GradeSystem gradeSystem;
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        double result=mockGradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
+//        verify(mockGradeService,times(1)).calculateAverageGrades();
     }
 }
